17c4d9bdc4693c321f59a1d166e6b67e83f627ce
Ward Cunningham <ward@c2.com>
2010-03-28 14:28:44 -0700

    live data for single channel

--- js/ssp.js
+++ js/ssp.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	var wave = [];
 	var n = 0;
+	var patt=/\b[abctr]\d+$/;
+	var chan=patt.exec(window.location.href);
 	$(document).everyTime(1000, function(i) {
 		$.getJSON("http://98.232.243.25:8082/p?callback=?", function(element){
 			if (wave.length >= 300) {wave.shift();}
@@ -10,7 +12,11 @@ $(document).ready(function(){
 				var x = wave[w][0];
 				var sample = wave[w][1];
 				for (i in sample) {
-					if(i.match(/t|r/)) {continue;}
+					if(chan!=null) {
+						if(i!=chan) {continue}
+					} else {
+						if(i.match(/t|r/)) {continue;}
+					}
 					if(data[i]==undefined) {data[i]=new Array(30);}
 					data[i].push([x, sample[i]]);
 				}
