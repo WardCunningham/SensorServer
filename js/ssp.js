af0294ec5ad670b5b4aa5e16c50b42130b474b49
Ward Cunningham <ward@c2.com>
2010-03-28 16:05:16 -0700

    convert live x-axis to real time

--- js/ssp.js
+++ js/ssp.js
@@ -5,12 +5,13 @@ $(document).ready(function(){
 	var chan=patt.exec(window.location.href);
 	$(document).everyTime(1000, function(i) {
 		$.getJSON("http://98.232.243.25:8082/p?callback=?", function(element){
+			if (n==0) {n = (new Date()).valueOf() - (element.t0 - element.t1);}
 			if (wave.length >= 300) {wave.shift();}
-			wave.push([n++,element]);
+			wave.push(element);
 			var data = {};
 			for (var w = 0; w<wave.length; w++) {
-				var x = wave[w][0];
-				var sample = wave[w][1];
+				var sample = wave[w];
+				var x = n + sample.t0 - sample.t1;
 				for (i in sample) {
 					if(chan!=null) {
 						if(i!=chan) {continue}
@@ -23,9 +24,9 @@ $(document).ready(function(){
 			}
 			var plot_data = [];
 			for (i in data) {
-				plot_data.push(data[i]);
+				plot_data.push({ label: i, data: data[i] });
 			}
-			$.plot($("#plot"), plot_data);
+			$.plot($("#plot"), plot_data, { xaxis: { mode: "time" }, legend: { position: "nw"} });
 			$("#stat").html(
 				"<br>elapsed time: " + element.t0/1000 +
 				"<br>requests served: " + element.r0 +
